db6ac22ffef597eb754981442f353eafd6a58014
victorlisita <victor.lisita20@ycdsbk12.ca>
2020-05-03 21:25:21 -0400

    create

--- .gitignore/robotics_create.py
+++ .gitignore/robotics_create.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 
 
-class Bin:
+class BaseRecord:
+    def __init__(self, created_at: datetime, updated_at: datetime, id: int):
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.id = id
+
+
+class Bin(BaseRecord):
     """ Represents a storage bin
     Attrs:
         bin_id(int): shows the id of the bin
@@ -9,15 +16,15 @@ class Bin:
         part_id(str): shows id of a single Part object
         qty_in_bin(int): shows how many of said Part is in the bin
     """
-    def __init__(self, bin_id: int, location: str, part_id: str, qty_in_bin: int):
+    def __init__(self, id: int, location: str, part_id: str, qty_in_bin: int, created_at, updated_at):
         "Initializes description of the bin"
-        self.id = bin_id
+        super().__init__(created_at, updated_at, id)
         self.location = location
         self.part_id = part_id
         self.qty_in_bin = qty_in_bin
 
 
-class Part:
+class Part(BaseRecord):
     """ Represents a part
     Attrs:
         part_id(int): shows the id of the part
@@ -25,27 +32,28 @@ class Part:
         quantity(int): show how much of that part is in the bin
         bin_id(int): show the id of a single Bin object
     """
-    def __init__(self, part_id: int, name: str, quantity: int, bin_id: int):
+    def __init__(self, id: int, name: str, quantity: int, bin_id: int, created_at, updated_at):
         "Initializes description of the part"
-        self.id = part_id
+        super().__init__(created_at, updated_at, id)
         self.name = name
         self.quantity = quantity
         self.bin_id = bin_id
 
 
-class User:
+class User(BaseRecord):
     """ Represents a User's info
     Attrs:
         email(str): shows the student's email
         student_num(int): shows the student's number
     """
-    def __init__(self, email: str, student_num: int):
+    def __init__(self, email: str, student_num: int, created_at, updated_at, id):
         "Initializes description of the student's information"
+        super().__init__(created_at, updated_at, id)
         self.email = email
         self.student_num = student_num
 
 
-class Log:
+class Log(BaseRecord):
     """ Represents a user login
     Attrs:
         time(datetime): shows the time of login
@@ -53,23 +61,24 @@ class Log:
         part_id(int): show's the id of the part being logged out
         quantity(int): shows how much of a part was logged out
     """
-    def __init__(self, time: datetime, user_id: int, part_id: int, quantity: int):
+    def __init__(self, user_id: int, part_id: int, quantity: int, created_at, updated_at, id):
         "Initializes description of the log data"
-        self.time = time
+        super().__init__(created_at, updated_at, id)
         self.user_id = user_id
         self.part_id = part_id
         self.quantity = quantity
 
 
-class InventoryManager:
+class InventoryManager(BaseRecord):
     """ Represents an inventory
     parts(List): Shows a list of all parts
     bins(List): Shows a list of all bins
     logs(List): Shows a list of all the logs
     users(List): Shows a list of all users
     """
-    def __init__(self):
+    def __init__(self, created_at, updated_at, id):
         "Initializes description of the inventory data"
+        super().__init__(created_at, updated_at, id)
         self.parts = []
         self.bins = []
         self.logs = []
